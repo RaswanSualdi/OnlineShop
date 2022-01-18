b189f47cc085ce5cf98243a22beed3c2bd8e8fa6
RaswanSualdi <you@example.com>
2022-01-18 17:11:12 +0800

    ini adalah repo file front end untuk toko online

--- src/components/HeroShayna.vue
+++ src/components/HeroShayna.vue
@@ -33,7 +33,7 @@
                 </div>
             </div>
 
-                    <div class="single-hero-items set-bg" style="background-image: url('/img/hero-3.jpg')">
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-3.jpg')">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-5">
